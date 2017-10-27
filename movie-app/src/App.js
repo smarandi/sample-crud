3558f6c4a8690c2837b283bf8e6431651c1a2159
samuel-marandi <samuel.marandi@evivehealth.com>
2017-10-28 03:32:48 +0530

    Initial setup

---
+++ movie-app/src/App.js
@@ -0,0 +1,16 @@
+import React,  { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class Splash extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    );
+  }
+}
+
+export default App = () => {
+  return (<Splash />);
+};
